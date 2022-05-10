27120ec6f577455f69f618f416404dc5bc8b205a
Emma Wang <emmaw@mit.edu>
2022-05-10 19:23:30 -0400

    implemented component, home page, and docs

--- src/StarRating.js
+++ src/StarRating.js
@@ -3,9 +3,73 @@ class StarRating extends HTMLElement {
     super();
     this.#render();
   }
+  #rating = 3;
+  #outof = null;
+  #emoji = null;
+
+  get rating() {
+    return this.#rating;
+  }
+  set rating(r) {
+    this.#rating = parseInt(r);
+    this.setAttribute("rating", parseInt(r));
+  }
+
+  get outof() {
+    return this.#outof;
+  }
+  set outof(o) {
+    this.#outof = parseInt(o);
+    this.setAttribute("outof", parseInt(o));
+  }
+
+  get emoji() {
+    return this.#emoji;
+  }
+  set emoji(e) {
+    this.#emoji = e;
+    this.setAttribute("emoji", e);
+  }
+
+  static get observedAttributes() {
+    return ["rating", "outof", "emoji"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "rating") {
+      this.#rating = parseInt(newValue);
+    } else if (name === "outof") {
+      this.#outof = parseInt(newValue);
+    } else if (name === "emoji") {
+      this.#emoji = newValue;
+    }
+    this.#render();
+  }
+
+  connectedCallback() {
+    this.#render();
+  }
+
   #render() {
-    console.log("hi");
-    this.innerHTML = "<p>hi</p>";
+    if (this.#emoji) {
+      this.innerHTML = this.#emoji.repeat(this.#rating);
+    } else {
+      const shadedStar = "★";
+      if (this.#outof) {
+        const emptyStar = "☆";
+        if (this.#rating <= this.#outof) {
+          this.innerHTML =
+            shadedStar.repeat(this.#rating) +
+            emptyStar.repeat(this.#outof - this.#rating);
+        } else {
+          this.innerHTML = shadedStar.repeat(this.#outof);
+          this.#rating = this.#outof;
+          this.setAttribute("rating", this.#outof);
+        }
+      } else {
+        this.innerHTML = shadedStar.repeat(this.#rating);
+      }
+    }
   }
 }
 
